a56db4273fbc0d825238e06f3e47f29167905c94
Jamin <4D3H2@tipped8.com>
2016-06-30 23:05:30 -0700

    1)))removed the spaces from the creation of the name variable 2)))added g to template sed command

--- findnew.sh
+++ findnew.sh
@@ -3,7 +3,7 @@
 cd $COINBIN
 for a in $(ls *d)
 do
-  name = $(echo $a|sed 's/.$//')
+  name=$(echo $a|sed 's/.$//')
   cp -v $COINSCRIPTS/.template.daemon $COINSCRIPTS/$name.daemon
-  sed -i "s/template/$name/" $COINSCRIPTS/$name.daemon
+  sed -i "s/template/$name/g" $COINSCRIPTS/$name.daemon
 done
