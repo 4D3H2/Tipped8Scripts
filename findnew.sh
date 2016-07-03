66c2af03ccc7051d442b20dd12b67c4a043c93d0
Jamin <4D3H2@tipped8.com>
2016-07-02 23:38:52 -0700

    1))) daemon finder now creates an executeable cli and tx script per the nessesity of certain coins 2))) added peerinfo scripts to keep track of active coin nodes

--- findnew.sh
+++ findnew.sh
@@ -5,13 +5,33 @@ cd $COINBIN
 for a in $(ls *d)
 do
   name=$(echo $a|sed 's/.$//')
-  if [ -f $COINSCRIPTS/$name.daemon ]
+  if [ -f $COINSCRIPTS/$name.tx ]
   then
 	continue
   fi
   cp -v $COINSCRIPTS/.template.daemon $COINSCRIPTS/$name.daemon
   sed -i "s/template/$name/g" $COINSCRIPTS/$name.daemon
   chmod +x $COINSCRIPTS/$name.daemon
+  if [ -f $COINBIN/$name-cli ]
+  then
+	cp -v $COINSCRIPTS/.template.cli $COINSCRIPTS/$name.cli
+  else
+	cp -v $COINSCRIPTS/.template.daemon $COINSCRIPTS/$name.cli
+  fi
+  if [ -f $COINBIN/$name-tx ]
+  then
+	cp -v $COINSCRIPTS/.template.tx $COINSCRIPTS/$name.tx
+  else
+	cp -v $COINSCRIPTS/.template.daemon $COINSCRIPTS/$name.tx
+  fi
+  sed -i "s/template/$name/g" $COINSCRIPTS/$name.cli
+  sed -i "s/template/$name/g" $COINSCRIPTS/$name.tx
+  chmod +x $COINSCRIPTS/$name.cli
+  chmod +x $COINSCRIPTS/$name.tx
+  if [ -d $HOME/.$name -a $COINDATA/$name ]
+  then
+	continue
+  fi
   mkdir $HOME/.$name
   mkdir $COINDATA/$name
   cp -v $COINDATA/.template.conf $HOME/.$name/$name.conf
